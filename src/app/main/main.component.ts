e9ebf960af1cb5635cdd8e908f3b1264c3e69d7a
Grischa <Grischa.Taenzer@gmail.com>
2024-06-26 22:07:37 +0200

    Add more components

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { ChannelListComponent } from "./channel-list/channel-list.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
-  selector: 'app-main',
-  standalone: true,
-  imports: [],
-  templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+    selector: 'app-main',
+    standalone: true,
+    templateUrl: './main.component.html',
+    styleUrl: './main.component.scss',
+    imports: [ChannelListComponent, HeaderComponent]
 })
 export class MainComponent {
 
