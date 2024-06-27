9b1e5322762374af2f41a240808ffe2d1070dc42
Grischa <Grischa.Taenzer@gmail.com>
2024-06-27 19:28:42 +0200

    Add material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,8 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideFirebaseApp(() => initializeApp({"projectId":"dabubble-f0943","appId":"1:199043385842:web:662b44ec62dedd082c436b","storageBucket":"dabubble-f0943.appspot.com","apiKey":"AIzaSyCwmM6PzC5sAJWu13TrEJEIuG2ZqDl-54A","authDomain":"dabubble-f0943.firebaseapp.com","messagingSenderId":"199043385842"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
+  providers: [provideRouter(routes), provideClientHydration(), provideFirebaseApp(() => initializeApp({"projectId":"dabubble-f0943","appId":"1:199043385842:web:662b44ec62dedd082c436b","storageBucket":"dabubble-f0943.appspot.com","apiKey":"AIzaSyCwmM6PzC5sAJWu13TrEJEIuG2ZqDl-54A","authDomain":"dabubble-f0943.firebaseapp.com","messagingSenderId":"199043385842"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideAnimationsAsync()]
 };
