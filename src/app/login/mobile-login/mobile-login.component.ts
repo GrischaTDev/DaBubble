2d004d3045365df2c8d45afa19ae255f5797af2f
SilasSchiel <silas-schiel@gmx.de>
2024-06-27 21:42:59 +0200

    mobile login styles

--- src/app/login/mobile-login/mobile-login.component.ts
+++ src/app/login/mobile-login/mobile-login.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-mobile-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './mobile-login.component.html',
   styleUrl: './mobile-login.component.scss'
 })
