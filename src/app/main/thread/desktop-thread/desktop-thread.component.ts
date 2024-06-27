9ca0e3084c8df10927fe9033b31cdfb992279041
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2024-06-27 20:29:01 +0200

    component thread mobile and desktop

---
+++ src/app/main/thread/desktop-thread/desktop-thread.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-desktop-thread',
+  standalone: true,
+  imports: [],
+  templateUrl: './desktop-thread.component.html',
+  styleUrl: './desktop-thread.component.scss'
+})
+export class DesktopThreadComponent {
+
+}
---
+++ src/app/main/thread/mobile-thread/mobile-thread.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-thread',
+  standalone: true,
+  imports: [],
+  templateUrl: './mobile-thread.component.html',
+  styleUrl: './mobile-thread.component.scss'
+})
+export class MobileThreadComponent {
+
+}
