04ef0386495f83e799c5963c48d07edeed7a7525
SilasSchiel <silas-schiel@gmx.de>
2024-06-27 22:17:59 +0200

    More styles for Login

--- src/app/login/mobile-login/mobile-login.component.ts
+++ src/app/login/mobile-login/mobile-login.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-mobile-login',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    MatInputModule,
+    MatIconModule,
+    MatFormFieldModule,
   ],
   templateUrl: './mobile-login.component.html',
   styleUrl: './mobile-login.component.scss'
