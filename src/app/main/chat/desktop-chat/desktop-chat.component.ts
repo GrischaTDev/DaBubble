69b7d873a8326cc496856ec7eff4346d483ff8c4
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2024-06-27 20:04:52 +0200

    chat component mobile and desktop

---
+++ src/app/main/chat/desktop-chat/desktop-chat.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-desktop-chat',
+  standalone: true,
+  imports: [],
+  templateUrl: './desktop-chat.component.html',
+  styleUrl: './desktop-chat.component.scss'
+})
+export class DesktopChatComponent {
+
+}
---
+++ src/app/main/chat/mobile-chat/mobile-chat.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-chat',
+  standalone: true,
+  imports: [],
+  templateUrl: './mobile-chat.component.html',
+  styleUrl: './mobile-chat.component.scss'
+})
+export class MobileChatComponent {
+
+}
