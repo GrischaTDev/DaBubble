7a8dd29c88141f9ee7d75a73485c9163d924d8af
Grischa <Grischa.Taenzer@gmail.com>
2024-06-27 21:38:09 +0200

    Update style.css and header mobile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,4 @@ import { MainComponent } from './main/main.component';
 export const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'main', component: MainComponent },
-    { path: 'mobile-channels', component: MainComponent },
 ];
