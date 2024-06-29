4c34be04f991d67b83e24f9dde2ac9a9f18a6ae8
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2024-06-29 19:04:13 +0200

    input field mobile chat

--- src/app/main/chat/mobile-chat/mobile-chat.component.ts
+++ src/app/main/chat/mobile-chat/mobile-chat.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-mobile-chat',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, FormsModule],
   templateUrl: './mobile-chat.component.html',
   styleUrl: './mobile-chat.component.scss'
 })
 export class MobileChatComponent {
+  text = '';
 
+  adjustHeight(eventTarget: EventTarget | null): void {
+    if (eventTarget instanceof HTMLTextAreaElement) {
+      const textarea = eventTarget;
+      textarea.style.overflow = 'hidden';
+      textarea.style.height = 'auto';
+      textarea.style.height = `${textarea.scrollHeight}px`;
+      window.scrollTo(0, document.body.scrollHeight);
+    }
+  }
 }
