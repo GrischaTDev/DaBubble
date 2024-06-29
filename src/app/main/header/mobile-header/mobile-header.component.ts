32bee49302d512c30be1cb35776bffaa333b3a12
Grischa <Grischa.Taenzer@gmail.com>
2024-06-29 14:04:04 +0200

    Add user popup menu

--- src/app/main/header/mobile-header/mobile-header.component.ts
+++ src/app/main/header/mobile-header/mobile-header.component.ts
@@ -1,13 +1,32 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-mobile-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './mobile-header.component.html',
-  styleUrl: './mobile-header.component.scss'
+  styleUrls: ['./mobile-header.component.scss'] 
 })
 export class MobileHeaderComponent {
+  userMenu: boolean = false;
 
+  @HostListener('document:click', ['$event'])
+  clickout(event: Event) {
+    const target = event.target as HTMLElement;
+    const triggerElement = document.querySelector('.user'); // Element, das openUserMenu() auslöst
+  
+    if (this.userMenu && target !== triggerElement) { // Nur schließen, wenn nicht auf triggerElement geklickt wurde
+      const clickedInsideMenu = target.closest('.menu-container');
+      if (!clickedInsideMenu) {
+        this.userMenu = false;
+      }
+    }
+  }
+  
+
+  openUserMenu() {
+    this.userMenu = !this.userMenu;
+  }
 }
