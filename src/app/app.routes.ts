673ce27bcec722edbc7540858afdf119e4a1bdee
Grischa <Grischa.Taenzer@gmail.com>
2024-06-30 19:21:59 +0200

    Update main components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login/login.component';
 import { MainComponent } from './main/main.component';
 import { RegisterComponent } from './login/register/register.component';
+import { MobileChatComponent } from './main/chat/mobile-chat/mobile-chat.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'main', component: MainComponent },
+    { path: 'chat', component: MobileChatComponent },
 ];
