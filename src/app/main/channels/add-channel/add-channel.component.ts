c4295472f9062bb9ee0a38238a405325fd8f6078
Grischa <Grischa.Taenzer@gmail.com>
2024-07-02 20:39:35 +0200

    Update add channel

--- src/app/main/channels/add-channel/add-channel.component.ts
+++ src/app/main/channels/add-channel/add-channel.component.ts
@@ -14,6 +14,7 @@ import { FormsModule } from '@angular/forms';
 export class AddChannelComponent {
   addUserMenu: boolean = false;
   newChannelName: string = '';
+  newChannelDescription: string = '';
 
   constructor(public dialogRef: MatDialogRef<AddChannelComponent>) {}
 
@@ -26,7 +27,7 @@ export class AddChannelComponent {
   }
 
   addChannel() {
-    
+    console.log(this.newChannelDescription);
   }
 
 }
