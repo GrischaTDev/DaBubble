738e3fec7092d26c5b9530782ddf7bb6a010f9b6
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2024-07-02 20:22:34 +0200

    message to user and channel class

--- src/assets/models/message.class.ts
+++ src/assets/models/message.class.ts
@@ -2,11 +2,13 @@ export class Message {
     nameUser: string;
     date: number;
     message: string;
+    answerMessage: Message[];
 
     constructor(obj?: any) {
       this.nameUser = obj ? obj.nameUser : '';
       this.date = obj ? obj.name: '';
       this.message = obj ? obj.email : '';
+      this.answerMessage = obj ? obj.answerMessage : [];
     }
   
     public toJSON() {
@@ -14,6 +16,7 @@ export class Message {
         nameUser: this.nameUser,
         date: this.date,
         message: this.message,
+        answerMessage: this.answerMessage
       };
     }
   }
