be176c4267a592f6790a6e80b59de305761bf9d2
SilasSchiel <silas-schiel@gmx.de>
2024-07-02 21:32:17 +0200

    add google login

--- src/app/login/shared/login-card/login-card.component.ts
+++ src/app/login/shared/login-card/login-card.component.ts
@@ -1,15 +1,70 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterModule } from '@angular/router';
+import { FirebaseApp } from '@angular/fire/app';
+import { getAuth, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { doc, Firestore, setDoc } from '@angular/fire/firestore';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink, RouterModule } from '@angular/router';
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { User } from '../../../../assets/models/user.class';
 
 @Component({
   selector: 'app-login-card',
   standalone: true,
   imports: [
-    RouterModule
+    RouterModule,
+    FormsModule,
   ],
   templateUrl: './login-card.component.html',
   styleUrl: './login-card.component.scss'
 })
 export class LoginCardComponent {
+
+  constructor( private firestore: Firestore, private router: Router ) {}
+  email: string = '';
+  password: string = '';
+
+  async login() {
+    const auth = getAuth();
+    await signInWithEmailAndPassword(auth, this.email, this.password)
+     .then(() => {
+       console.log('Dieser Login war erfolgreich');
+       this.router.navigate(['main']);
+       
+     })
+     .catch(() => {
+        console.log('Dieser Login ist fehlgeschlagen');
+     })
+
+  }
+
+  async loginWithGoogle() {
+
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+
+    signInWithPopup(auth, provider) 
+     .then(async (result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      if(credential) {
+        const token = credential.accessToken;
+        console.log(token);
+
+        const user = result.user;
+        console.log(user);
+
+        const newUser = new User({
+          idUser: user.uid,
+          name: user.displayName,
+          email: user.email,
+          avatar: user.photoURL
+        });
+
+        const userRef = doc(this.firestore, 'users', user.uid);
+        await setDoc(userRef, newUser.toJSON());
+        this.router.navigate(['main']);
+
+      }
+     })
+  }
 
 }
