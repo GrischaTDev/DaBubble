8a0df32dfcb707c84fe01ace03f4d68e3569f326
SilasSchiel <silas-schiel@gmx.de>
2024-07-25 20:27:43 +0200

    First styles for thread

--- src/app/main/thread/mobile-thread/mobile-thread.component.ts
+++ src/app/main/thread/mobile-thread/mobile-thread.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { MobileChatHeaderComponent } from '../../header/mobile-chat-header/mobile-chat-header.component';
+import { MatIcon } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { ChatService } from '../../../service/chat.service';
 
 @Component({
   selector: 'app-mobile-thread',
   standalone: true,
-  imports: [],
+  imports: [
+    MobileChatHeaderComponent, 
+    MatIcon,
+    FormsModule,
+  ],
   templateUrl: './mobile-thread.component.html',
   styleUrl: './mobile-thread.component.scss'
 })
 export class MobileThreadComponent {
+
+  parmsId: string = '';
+
+  constructor(public chatService: ChatService) {}
 
 }
