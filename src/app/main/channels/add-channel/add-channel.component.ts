a39700c2cd32afc288546e70bcb9b2b7f49f3c0e
Grischa <Grischa.Taenzer@gmail.com>
2024-07-28 22:01:47 +0200

    add channel update user add

--- src/app/main/channels/add-channel/add-channel.component.ts
+++ src/app/main/channels/add-channel/add-channel.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, HostListener, NgModule } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, HostListener, Output, EventEmitter, NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
@@ -8,7 +8,7 @@ import { Channel } from '../../../../assets/models/channel.class';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { ChatService } from '../../../service/chat.service';
 import { User } from '../../../../assets/models/user.class';
-
+import { ChannelService } from '../../../service/channel.service';
 
 @Component({
   selector: 'app-add-channel',
@@ -17,7 +17,7 @@ import { User } from '../../../../assets/models/user.class';
   templateUrl: './add-channel.component.html',
   styleUrl: './add-channel.component.scss',
 })
-export class AddChannelComponent implements OnInit{
+export class AddChannelComponent implements OnInit {
   addUserMenu: boolean = false;
   isAddUserMenuOpen: boolean = false;
   addUserInput: boolean = false;
@@ -26,14 +26,107 @@ export class AddChannelComponent implements OnInit{
   dataChannel: Channel = new Channel();
   isDesktop: boolean = false;
   isThreadOpen: boolean = false;
+  @Output() contentChange = new EventEmitter<string>();
 
 
   constructor(
     private breakpointObserver: BreakpointObserver,
     public dialogRef: MatDialogRef<AddChannelComponent>,
     public mainService: MainServiceService,
-    public ChatService: ChatService
-  ) {}
+    public ChatService: ChatService,
+    public channelService: ChannelService,
+    private cdr: ChangeDetectorRef
+  ) {
+    this.channelService.pushUserToEditList();
+  }
+
+
+  /**
+ * Activates the user search interface and triggers change detection.
+ * This method is called when the user initiates a search for adding a new user.
+ */
+  openSearchUser() {
+    this.channelService.addUserClicked = true;
+    this.cdr.detectChanges();
+  }
+
+
+  /**
+* Updates the content based on user input, filters user-related content, and emits an event if the content is not empty.
+* @param {Event} [event] - Optional event parameter that can be used to pass event data.
+*/
+  updateContent(event?: Event): void {
+    this.updateIsEmpty(this.channelService.content);
+    this.channelService.filterUserContent(this.channelService.content);
+    if (!this.channelService.isEmpty) {
+      this.contentChange.emit(this.channelService.content);
+    }
+  }
+
+  /**
+   * Updates the `isEmpty` state of the content based on its presence or if it matches the placeholder.
+   * @param {string} [content] - Optional content string to check against the placeholder for emptiness.
+   */
+  updateIsEmpty(content?: string,) {
+    this.channelService.isEmpty = !content || content === this.channelService.placeholder;
+  }
+
+  /**
+   * Clears the content if it is empty when the component gains focus.
+   * This method responds to the `focusin` event to manage content initialization.
+   */
+  @HostListener('focusin')
+  onFocusIn() {
+    if (this.channelService.isEmpty) {
+      this.channelService.content = '';
+    }
+  }
+
+  /**
+   * Sets the content to a placeholder if it is empty or emits the current content if not, when the component loses focus.
+   * This method responds to the `focusout` event to ensure the content is appropriately managed when focus is lost.
+   */
+  @HostListener('focusout')
+  onFocusOut() {
+    if (this.channelService.isEmpty) {
+      this.channelService.content = this.channelService.placeholder;
+    } else {
+      this.contentChange.emit(this.channelService.content);
+    }
+  }
+
+  /**
+  * Adds a selected user to the added users list, removes them from the edit list, and clears the content.
+  * It also updates the component state and UI, such as checking if the content is empty and adjusting the contenteditable element's height.
+  * @param {User} userFilter - The user object to add to the added users list.
+  * @param {number} indexFilterUser - The index of the user in the edit user list to remove.
+  * @param {Event} event - The event object, typically from a user interaction.
+  */
+  pushAddetUser(userFilter: User, indexFilterUser: number, event: Event) {
+    this.channelService.editUserList.splice(indexFilterUser, 1);
+    this.channelService.addetUser.push(userFilter);
+    this.channelService.content = '';
+    this.updateContent(event);
+    this.updateIsEmpty(this.channelService.content);
+    this.channelService.filterUserContent(this.channelService.content);
+    if (!this.channelService.isEmpty) {
+      this.contentChange.emit(this.channelService.content);
+    }
+    const editableDiv = document.querySelector('[contenteditable="true"]');
+    if (editableDiv) editableDiv.innerHTML = '';
+    this.calculateHeight();
+  }
+
+
+  /**
+* Increases the height of the textarea by a set increment and triggers change detection to update the view.
+* This method is typically used to adjust the layout dynamically based on content or interaction changes.
+*/
+  calculateHeight() {
+    this.channelService.textareaHeight = this.channelService.textareaHeight + 37;
+    this.channelService.filterContentMarginTop = this.channelService.filterContentMarginTop + 37;
+    this.cdr.detectChanges();
+  }
 
 
   closeDialog() {
@@ -43,7 +136,7 @@ export class AddChannelComponent implements OnInit{
     }, 2000);
   }
 
-  
+
   doNotClose(event: Event) {
     event.stopPropagation();
   }
@@ -52,7 +145,7 @@ export class AddChannelComponent implements OnInit{
   openAddUserMenu(event: Event) {
     event.stopPropagation();
     this.addUserMenu = !this.addUserMenu;
-    if(this.isDesktop) {
+    if (this.isDesktop) {
       this.isAddUserMenuOpen = true;
     }
   }
@@ -74,10 +167,10 @@ export class AddChannelComponent implements OnInit{
    * and adding it to Firebase.
    */
   addChannel() {
-    this.closeDialog();  
+    this.closeDialog();
     this.dataChannel.name = this.newChannelName;
     this.dataChannel.description = this.newChannelDescription;
-    this.dataChannel.messageToMe = false;   
+    this.dataChannel.messageToMe = false;
     this.dataChannel.ownerUser.push(new User(this.mainService.loggedInUser));
     this.mainService.addNewDocOnFirebase(
       'channels',
